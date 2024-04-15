c73e45ae268e0872967318887aeaae06cbb27835
Pierre <pierre.yvenou@orange.fr>
2024-04-15 10:48:29 +0200

    init pt1000

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { NavigationContainer } from '@react-navigation/native'; import { createS
 const Stack = createStackNavigator(); LogBox.ignoreAllLogs(true);
 import HomeSceen from "./app/screens/home.screen";
 import {SocketContext, socket} from "./app/contexts/socket.context";
+import OnlineGameScreen from "./app/screens/online-game.screen";
+import VsBotGameScreen from "./app/screens/vs-bot-game.screen";
 function App() { return (
     <SocketContext.Provider value={socket}>
         <NavigationContainer>
             <Stack.Navigator initialRouteName="HomeScreen">
                 <Stack.Screen name="HomeScreen" component={HomeScreen} />
+                <Stack.Screen name="OnlineGameScreen" component={OnlineGameScreen} />
+                <Stack.Screen name="VsBotGameScreen" component={VsBotGameScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     </SocketContext.Provider>
--- app/screens/home.screen.js
+++ app/screens/home.screen.js
@@ -1,51 +1,24 @@
-import { useEffect, useState, useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { SocketContext } from '../contexts/socket.context';
-export default function HomeScreen() {
-    const socket = useContext(SocketContext);
-    const [time, setTime] = useState(null);
-
-    useEffect(() => {
-        socket.on("time-msg", (data) => { setTime(new Date(data.time).toString());});
-        return () => {
-            socket.disconnect();
-            socket.removeAllListeners();
-        };
-    }, []);
-
+import { StyleSheet, View, Button } from "react-native";
+export default function HomeScreen({ navigation }) {
     return (
         <View style={styles.container}>
-            {!socket ? (
-                <>
-                    <Text style={styles.paragraph}>
-                        Connecting to websocket server...
-                    </Text>
-                    <Text style={styles.footnote}>
-                        Make sure the backend is started and reachable
-                    </Text> </>
-            ):
-            socket && (
-                <>
-                    <Text style={[styles.paragraph, { fontWeight: "bold" }]}>
-                        Server time
-                    </Text>
-                    <Text style={styles.paragraph}>{time}</Text>
-                </>
-            )} </View>
+            <View>
+                <Button
+                    title="Jouer en ligne"
+                    onPress={() => navigation.navigate('OnlineGameScreen')}
+                />
+            </View>
+            <View>
+                <Button
+                    title="Jouer contre le bot"
+                    onPress={() => navigation.navigate('VsBotGameScreen')}
+                />
+            </View>
+        </View>
     ); }
-
-const styles = StyleSheet.create({
-    container: {
+const styles = StyleSheet.create({ container: {
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "center",
-    },
-    paragraph: {
-        fontSize: 16,
-    },
-    footnote: {
-        fontSize: 14,
-        fontStyle: "italic",
-    },
-});
+    } });
