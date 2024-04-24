acbc12aa50c0e1e98eed063a1764f43708d10d29
icewill <alexis.bertin01@gmail.com>
2024-04-24 08:24:29 +0200

    start grid

--- app/contexts/socket.context.js
+++ app/contexts/socket.context.js
@@ -3,7 +3,7 @@ import React from "react";
 import { Platform } from 'react-native'; import io from "socket.io-client";
 console.log('Emulation OS Platform: ', Platform.OS);
 // Also usable : "http://10.0.2.2:3000"
-export const socketEndpoint = Platform.OS === 'web' ? "http://localhost:3000" : "http://172.20.10.3:3000";
+export const socketEndpoint = Platform.OS === 'web' ? "http://localhost:3000" : "http://10.60.104.126:3000";
 export const socket = io(socketEndpoint, { transports: ["websocket"],
 });;
 export let hasConnection = false;
